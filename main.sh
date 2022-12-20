d07e5f6269445cf34b056db5ea6bad8af7c92ecf
hkbase <hkbase@163.com>
2022-12-20 10:29:33 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 cp -f ./config.yaml /tmp/config.yaml
 sed -i "s|PASSWORD|${PASSWORD}|g;s|WSPATH|${WSPATH}|g" /tmp/config.yaml
+PATH_IN_LINK=$(echo ${WSPATH} | sed "s|\/|\%2F|g")
+    echo trojan://"${PASSWORD}@${REPL_SLUG}.${REPL_OWNER}.repl.co:443?security=tls&type=ws&path=${PATH_IN_LINK}#Replit" >/tmp/link
+echo "请使用v2rayNg客户端扫描下方二维码："
+qrencode -t ansiutf8 < /tmp/link
 xray run -c /tmp/config.yaml
-
