0a3dc6c5c419ae42c621c34b46189d9b26890a49
hkbase <hkbase@163.com>
2022-12-22 12:22:16 +0800

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,12 +1,13 @@
 { pkgs }: {
     deps = [
-        pkgs.htop
+        pkgs.bash
         pkgs.qrencode.bin
         pkgs.jq.bin
         pkgs.busybox
         pkgs.bashInteractive
         pkgs.xray
         pkgs.dig
+        pkgs.htop
         pkgs.dnsproxy
         pkgs.dogdns
     ];
