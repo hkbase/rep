d0449b126f76a6e9b1fe1612488677e3c3d6d852
hkbase <hkbase@163.com>
2023-02-06 10:52:07 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,4 +8,7 @@ qrencode -t ansiutf8 < /tmp/link
 wget -qO /tmp/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat 
 wget -qO /tmp/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
 dnsproxy -l 127.0.0.1 -p 53 -u tls://8.8.4.4 -u tcp://8.8.8.8 -u tcp://8.8.4.4 --all-servers -r 0 --cache --cache-optimistic --cache-min-ttl=7200 --ipv6-disabled -o /dev/null &
-xray run -c /tmp/config.yaml
+/home/runner/${REPL_SLUG}/xray run -c /tmp/config.yaml
+while true; do
+curl https://${REPL_SLUG}.${REPL_OWNER}.repl.co;sleep 300
+done
