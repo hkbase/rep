1c50880e56595946502a0d66160bce2a3ac9b6ed
hkbase <hkbase@163.com>
2023-02-28 10:11:24 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,3 +2,6 @@
 cp -f ./config.yaml /tmp/config.yaml
 sed -i "s|PASSWORD|${PASSWORD}|g;s|WSPATH|${WSPATH}|g" /tmp/config.yaml
 xray run -c /tmp/config.yaml
+while true; do
+curl https://${REPL_SLUG}.${REPL_OWNER}.repl.co;sleep 300
+done
