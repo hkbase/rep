d2aaadf0fe4dfd5bfe6bb697f9a4a3a6c69277c7
hkbase <hkbase@163.com>
2023-04-18 15:51:57 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 cp -f ./config.yaml /tmp/config.yaml
 sed -i "s|PASSWORD|${PASSWORD}|g;s|WSPATH|${WSPATH}|g" /tmp/config.yaml
-xray run -c /tmp/config.yaml &
-while true; do
-curl https://${REPL_SLUG}.${REPL_OWNER}.repl.co -s -o /dev/null
-sleep 90
-done
+xray run -c /tmp/config.yaml
